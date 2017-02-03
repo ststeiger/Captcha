ecdc9e94de096e2c4d81133f62a991fab747bf0c
ststeiger <quandary82@hailmail.net>
2017-02-03 18:27:06 +0100

    Resolution correection for SSRS PDF

--- Captcha/Program.cs
+++ Captcha/Program.cs
@@ -16,7 +16,26 @@ namespace Captcha
         [System.STAThread]
         static void Main()
         {
-            PasswordGenerator.Test();
+            // PasswordGenerator.Test();
+
+            string fileName = @"D:\username\Downloads\SNB-Logo-positiv-hochaufloesend.png";
+            // fileName = @"D:\username\Documents\Visual Studio 2008\Projects\SNB_Berichte\SNB_Berichte\logo_text_schwarz_klein.jpg";
+            // fileName = @"D:\username\Documents\Visual Studio 2008\Projects\SNB_Berichte\SNB_Berichte\SNB-Logo-positiv-hochaufloesend.png";
+            fileName = @"D:\username\Documents\Visual Studio 2008\Projects\SNB_Berichte\SNB_Berichte\snb_bns.png";
+            fileName = @"D:\username\Downloads\snb_bns_hoch.png";
+            fileName = @"D:\username\Documents\Visual Studio 2008\Projects\SNB_Berichte\SNB_Berichte\snb_bns_hoch.png";
+
+            using (System.Drawing.Bitmap bitmap = (System.Drawing.Bitmap)System.Drawing.Image.FromFile(fileName))
+            {
+                using (System.Drawing.Bitmap newBitmap = new System.Drawing.Bitmap(bitmap))
+                {
+                    //newBitmap.SetResolution(300, 300);
+                    newBitmap.SetResolution(96, 96);
+                    //newBitmap.SetResolution(1200, 1200);
+                    newBitmap.Save("snb_bns_96.png", System.Drawing.Imaging.ImageFormat.Png);
+                }
+            }
+
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
